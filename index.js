6d38ff7935057cc6bbf5ec0a072f8c205f597d37
BriceEugene <Eugene.supersenior@gmail.com>
2022-10-12 23:20:58 -0400

    updated

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-const path = require("path");
+// const path = require("path");
+// const express = require("express");
+// const app = express();
+// const publicPath = path.join(__dirname, "build");
+// const port = process.env.PORT || 3000;
+// app.use(express.static(publicPath));
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(publicPath, "index.html"));
+// });
+// app.listen(port, () => {
+//   console.log("Server is up!");
+// });
+
 const express = require("express");
-const app = express();
-const publicPath = path.join(__dirname, "build");
+const serveStatic = require("serve-static");
+const path = require("path");
+app = express();
+app.use(serveStatic(path.join(__dirname, "build")));
 const port = process.env.PORT || 3000;
-app.use(express.static(publicPath));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(publicPath, "index.html"));
-});
-app.listen(port, () => {
-  console.log("Server is up!");
-});
+app.listen(port);
